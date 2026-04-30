ef376718df965395b9a0fe12d6716d21f7af2637
Hector De Leon <antonioidon@hotmail.com>
2026-04-30 14:52:20 -0500

    Inputs mejorados para continuar, prints mejorados de resultados, control de flujo para continuar si no se introduce un valor valido. Proyecto terminado con posibles mejoras opcionales

--- Python/practica calculadora.py
+++ Python/practica calculadora.py
@@ -1,7 +1,10 @@
-practica = True
-while practica:
+practica = False
+while not practica:
     # Solicita al usuario el primer numero y lo convierte a decimal.
     try:
         numero1 = float(input("Digite el primer numero: "))
+        practica = True
     except:
         print("Error: Ingrese un numero valido.")
+
+print("Gracias por usar la calculadora. ¡Hasta luego!").lower()
